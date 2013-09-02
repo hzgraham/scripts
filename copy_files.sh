56ef4a7f258e8abe4e76a4ef7eec63b18af52259
Henry Graham <hgraham@redhat.com>
2013-09-02 00:27:28 -0400

    adding variables for the source and destination folders

--- copy_files.sh
+++ copy_files.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 #script designed to perform an md5sum check of files being copied
 
+read -p "Input where the source is mounted: " source
+read -p "Input where the destination is mounted: " destination
 
-rm /movies/md5check.txt
-rm /movies/md5checkreceiving.txt
-rm /movies/checkincoming.txt
-rm /movies/checksource.txt
+rm /tmp/md5check.txt
+rm /tmp/md5checkreceiving.txt
+rm /tmp/checkincoming.txt
+rm /tmp/checksource.txt
 for each in `cat ~/files.to.copy`
 do
-cd /movies/
-md5sum /costamovies/$each >> /movies/md5check.txt
+cd $destination
+md5sum $source/$each >> $destination/md5check.txt
 md5sum --check md5check_incoming.txt
 
-cp /costamovies/$each /movies
+cp $source/$each $destination
 
-md5sum /movies/$each >> /movies/md5checkreceiving.txt
+md5sum $destination/$each >> $destination/md5checkreceiving.txt
 md5sum --check md5checkreceiving.txt
 
-cat /movies/md5checkreceiving.txt | cut -d" " -f1 > /movies/check_destination.txt
-cat /movies/md5check_source.txt | cut -d" " -f1 > /movies/checksource.txt
+cat $destination/md5checkreceiving.txt | cut -d" " -f1 > $destination/check_destination.txt
+cat $destination/md5check_source.txt | cut -d" " -f1 > $destination/checksource.txt
 
 done
 
 
-#sed -r 's/^(\S+\s+){2}//' /movies/md5checkreceiving.txt
-#sed -r 's/^(\S+\s+){2}//' /movies/md5check.txt
+#sed -r 's/^(\S+\s+){2}//' $destination/md5checkreceiving.txt
+#sed -r 's/^(\S+\s+){2}//' $destination/md5check.txt
 
-diff /movies/checksource.txt /movies/check_destination.txt
+diff $destination/checksource.txt $destination/check_destination.txt
 
 exit 0
